a49542df546fdf610683ab41bc1505e416c15d46
User <sergioaotero26@hotmail.com>
2022-12-22 16:14:21 -0500

    creando botones

--- js/observation.js
+++ js/observation.js
@@ -2,8 +2,19 @@ var observation = function(){
     this.sessions = JSON.parse(localStorage.getItem("observadorSessionsInfo"))
     this.actualSession = sessions[localStorage.getItem("actualSessionInfo")];
     this.sessionName = ko.observable(actualSession["name"]);
-    this.subjects = this.actualSession["subjects"];
-    console.log(this.subjects)
+    // this.subjects = this.actualSession["subjects"];
+    // this.categories = this.actualSession["categories"];
+    this.subjects = {"messi" : {"parpadea" : 0, "come" : 0}, "sergio" : {"parpadea" : 0}}
+    this.names = Object.keys(this.subjects);
+
+    getCategories = function(subject){
+        return Object.keys(this.subjects[subject]);
+    }
+    // categories.forEach(categorie => {
+    //     for (i = 0; i < this.subjects.length - 1; i++){
+    //         this.categories.push(categorie);
+    //     }
+    // });
 }
 ko.applyBindings(observation);
 
